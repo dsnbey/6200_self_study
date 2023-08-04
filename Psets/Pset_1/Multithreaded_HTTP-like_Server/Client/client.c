ab874de75fff713bbf728cbe8d08ca986ada3300
dsn <gorkemaslandsn@gmail.com>
2023-08-04 23:09:35 +0300

    Version 6 works with PUT request. Logging may be improved. Add DELETE request and start with multithreading. Also, I find the networking pretty slow. Maybe IDE does not show the files as fast as it is created and written. A simple timing system can be implemented.

---
+++ Psets/Pset_1/Multithreaded_HTTP-like_Server/Client/client.c
@@ -0,0 +1,215 @@
+// 7 hours so far-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define MESSAGE_SIZE 256
+#define BUFFER_SIZE 1024
+const char NOT_FOUND = '0';
+const char OK = '1';
+const char INTERNAL_ERROR = '2';
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void* get(const char* filename, int socket);
+void* put(const char* filename, int socket);
+void* delete(const char* filename, int socket);
+
+
+int main(int argc, char* argv[]){
+
+    // declarations
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr; // address of the server to connect
+    struct hostent *server; // defines a host computer on the internet
+
+    if (argc < 3) {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+
+    // contains the name of a host on the Internet
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    /*
+     * Set fields in serv_addr
+     */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+
+    while (1) {
+        char buffer[BUFFER_SIZE];
+
+        printf("Enter the option for the server (0-> get, 1-> put, 2-> delete):  ");
+        scanf(" %c", &buffer[0]); // Note the space before %c to consume any leading whitespace
+
+        printf("Enter the filename to perform the operation: ");
+        scanf("%255s", &buffer[2]); // Start writing from the third element
+
+        // Add the ':' character as the second element in the buffer
+        buffer[1] = ':';
+
+        n = write(sockfd,buffer,strlen(buffer));
+        if (n < 0) {
+            error("ERROR writing to socket");
+            continue;
+        }
+
+
+
+        switch (buffer[0]) {
+            case '0':
+                get(&buffer[2], sockfd);
+                break;
+            case '1':
+                put(&buffer[2], sockfd);
+                break;
+            case '2':
+                delete(&buffer[2], sockfd);
+                break;
+
+        }
+
+        bzero(buffer,256);
+
+    }
+}
+
+void* get(const char* filename, int socket) {
+    // Send the 'get' command and filename to the server
+    char buffer[MESSAGE_SIZE] = {0};
+    buffer[0] = 0; // just for further readability
+    strncpy(&buffer[2], filename, MESSAGE_SIZE - 3);
+    if (send(socket, buffer, strlen(buffer), 0) == -1) {
+        perror("Send error");
+        return 0;
+    }
+
+    // Receive file size from server
+    size_t file_size;
+    if (recv(socket, &file_size, sizeof(file_size), 0) <= 0) {
+        perror("Receive error");
+        return 0;
+    }
+
+    if (file_size == 0) {
+        perror("File not found");
+        return 0;
+    }
+
+    // Open the file to write the received data
+    FILE* file = fopen(filename, "w");
+    if (!file) {
+        perror("Failed to open file");
+        return 0;
+    }
+
+    // Receive file data from server
+    size_t total_bytes_received = 0;
+    while (total_bytes_received < file_size) {
+        char file_buffer[BUFFER_SIZE];
+        ssize_t bytes_received = recv(socket, file_buffer, BUFFER_SIZE, 0);
+
+        if (bytes_received <= 0) {
+            perror("Receive error");
+            break;
+        }
+
+        // Write received data to file
+        fwrite(file_buffer, 1, bytes_received, file);
+        total_bytes_received += bytes_received;
+    }
+
+    // Close the file
+    fclose(file);
+}
+
+/*
+ * In a production code, put should only send the changes, not the whole file.
+ * However, this is no production, this code is for learning purposes.
+ * Therefore client will send the whole file and server will DELETE the file and RECREATE IT.
+ * YES STUPIDITY, LESS GOOO BRRRR!!!
+ */
+void* put(const char* filename, int socket) {
+
+    printf("PUT called");
+
+    FILE* file = fopen(filename, "r");
+
+    if (!file) {
+        perror("File not found in client.");
+        return 0;
+    }
+
+
+    // Send the filesize
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+
+    send(socket, &file_size, sizeof(file_size), 0);
+
+
+
+    //  Receive the status code
+    char status[1];
+    recv(socket, status, 1, 0);
+    if (status[0] == INTERNAL_ERROR) {
+        perror("Internal error occurred in the server. Please try it another time.");
+        return 0;
+    } else{
+        printf("Request received successfully by the server.");
+    }
+
+
+    char buffer[BUFFER_SIZE];
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file))) {
+        size_t total_bytes_send = 0;
+
+        while (total_bytes_send < bytes_read) {
+            size_t bytes_send = send(socket, buffer + total_bytes_send, bytes_read - total_bytes_send, 0);
+
+            if (bytes_send == -1) {
+                perror("Send error");
+                return 0;
+            }
+            total_bytes_send += bytes_send;
+        }
+    }
+    printf("All data should be successfully received.");
+    fclose(file);
+
+
+}
+void* delete(const char* filename, int socket) {
+    return 0;
+}
+
--- Psets/Pset_1/Multithreaded_HTTP-like_Server/Server/server.c
+++ Psets/Pset_1/Multithreaded_HTTP-like_Server/Server/server.c
@@ -1,4 +1,4 @@
-// 3 hours so far-
+// 7 hours so far-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,9 +12,6 @@
 const char NOT_FOUND = '0';
 const char OK = '1';
 const char INTERNAL_ERROR = '2';
-const char PUT_REPLACED = '3';
-const char PUT_CREATED = '4';
-const char DELETED = '5';
 
 void error(const char *msg)
 {
@@ -70,7 +67,7 @@ int main(int argc, char *argv[]) {
     // SERVER PROTOCOL
 
     int option;
-    char input[MESSAGE_SIZE]; // 0:MESSAGE
+    char input[MESSAGE_SIZE]; // structure: 0:MESSAGE
 
     while (1) {
         bzero(input, MESSAGE_SIZE);
@@ -79,17 +76,18 @@ int main(int argc, char *argv[]) {
             continue;
         } else {
             option = input[0];
+            printf("Option: %c", option);
 
             const char* message = &input[2];
 
             switch (option) {
-                case 0:
+                case '0':
                     get(message, newsockfd);
                     break;
-                case 1:
+                case '1':
                     put(message, newsockfd);
                     break;
-                case 2:
+                case '2':
                     delete(message, newsockfd);
                     break;
 
@@ -101,12 +99,21 @@ int main(int argc, char *argv[]) {
 }
 
 void* get(const char* filename, int socket) {
+
     FILE* file = fopen(filename, "r");
 
     if (!file) {
         send(socket, &NOT_FOUND, 1, 0);
     }
     else {
+        // Determine file size
+        fseek(file, 0L, SEEK_END);
+        size_t file_size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+
+        // Send file size to client
+        send(socket, &file_size, sizeof(file_size), 0);
+
         char buffer[BUFFER_SIZE];
         size_t bytes_read;
 
@@ -123,6 +130,68 @@ void* get(const char* filename, int socket) {
                 total_bytes_send += bytes_send;
             }
         }
+
+        fclose(file);
+    }
+}
+
+/*
+ * If you read this code and say "What a stupidity!" Check out comments in the client code.
+ * I know it's inefficient. I know I should only transfer the changes in the file.
+ * I don't care.
+ * I'm not building an efficient filesystem or change tracker.
+ * I'm learning to send stuff.
+ *
+ * BTW Code logic is same in the GET method of client.
+ */
+void* put(const char* filename, int socket) {
+
+    printf("PUT called on %s", filename);
+    char status[1];
+
+    // Receive file size from client.
+    size_t file_size;
+    if (recv(socket, &file_size, sizeof(file_size), 0) <= 0) {
+        perror("Receive error");
+        status[0] = INTERNAL_ERROR;
+        send(socket, status, 1, 0);
+        return 0;
     }
 
+    // Send success
+    status[0] = OK;
+    send(socket, status, 1, 0);
+
+    // HELL YEA STUPID CODE BEGINS
+    // DELETE THE FILE, RECREATE IT AND WRITE NEW STUFF
+    // BOOM
+
+    FILE* file = fopen(filename, "w+"); // OFC not that stupid, truncate it instead of deleting and recreating.
+    if (!file) {
+        perror("Failed to open file");
+        return 0;
+    }
+
+
+    // Receive file data from server
+    size_t total_bytes_received = 0;
+    while (total_bytes_received < file_size) {
+        char file_buffer[BUFFER_SIZE];
+        ssize_t bytes_received = recv(socket, file_buffer, BUFFER_SIZE, 0);
+
+        if (bytes_received <= 0) {
+            perror("Receive error");
+            break;
+        }
+
+        // Write received data to file
+        fwrite(file_buffer, 1, bytes_received, file);
+        total_bytes_received += bytes_received;
+    }
+
+    // Close the file
+    fclose(file);
+}
+void* delete(const char* filename, int socket) {
+    return 0;
 }
